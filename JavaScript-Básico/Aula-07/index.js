451e2bee872b59ef5620de0fc9da59a57fd2944c
Wadson-Ferreira <dev.wadson@gmail.com>
2025-12-30 11:45:58 -0300

    feat(aula-07) Implementa exemplos e estudos de const

---
+++ JavaScript-Básico/Aula-07/index.js
@@ -0,0 +1,26 @@
+/* 
+const nome = "João";
+console.log(nome);
+
+nome = "Maria"; // Isso causará um erro porque 'nome' foi declarado como uma constante
+console.log(nome);
+*/
+
+const texto = "Olá, mundo!";
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+let resultadoTriplicado = resultado * 3;
+console.log(resultadoTriplicado);
+
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+console.log(primeiroNumero + segundoNumero);
+
+console.log( typeof(primeiroNumero) );
+console.log( typeof(texto) );
