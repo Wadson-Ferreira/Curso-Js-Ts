a16d5784ab487095fb920f09eefe6dbb521e952f
Wadson-Ferreira <dev.wadson@gmail.com>
2025-12-29 22:34:07 -0300

    feat(aula-06): implementa exemplo de let

---
+++ JavaScript-Básico/Aula-06/index.js
@@ -0,0 +1,13 @@
+//let nome = 'João';//String
+let nome;
+nome = `João`;// Inicializando a variável
+
+console.log(nome);
+nome = 'Otávio';//Reatribuindo valor
+console.log(nome);
+ 
+console.log(`${nome} nasceu em 1984`);
+console.log(`Em 200 ${nome} conheceu Maria`);
+console.log(`${nome} casou-se com Maria em 2012.`);
+console.log(`Maria teve um filho com ${nome} em 2015.`);
+console.log(`O filho de ${nome} se chama Eduardo.`);
