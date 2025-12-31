eefd8cf9169e04236f88e8a686e35d4977e578c3
Wadson-Ferreira <dev.wadson@gmail.com>
2025-12-31 11:38:59 -0300

    feat (aula-09) implementa exemplos de tipos de dados primitivos

--- JavaScript-Básico/Aula-09/index.js
+++ JavaScript-Básico/Aula-09/index.js
@@ -1,6 +1,19 @@
-var nome = 'Luiz';
+/* var nome = 'Luiz';
 var nome = ' Otávio';
 const nome = 'otávio';
 
 let nome = 'Luiz' //Causa erro pois estamos redeclarando uma variável e let não permite esse comportamento.
 console.log(nome);
+*/
+
+const nome = 'Luiz'; //String
+const nome1 = "Luiz"; //String
+const nome2 = `Luiz`; //String
+
+const num1 = 10; //Number
+const num2 = 10.52; //Number
+let nomeAluno; // undefined -> Não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta para nenhum local na memória
+const boolean = true; // Tem somente dois valores possíveis, true e false. (é um valor lógico)
+
+console.log(typeof nome, nome);
