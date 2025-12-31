af4cb70cf9be46b8e5dfa91ab5ed1f4b102b5a60
Wadson-Ferreira <dev.wadson@gmail.com>
2025-12-31 10:18:17 -0300

    feat (aula-09) implementa exemplos de var, let e const

---
+++ JavaScript-Básico/Aula-09/index.js
@@ -0,0 +1,6 @@
+var nome = 'Luiz';
+var nome = ' Otávio';
+const nome = 'otávio';
+
+let nome = 'Luiz' //Causa erro pois estamos redeclarando uma variável e let não permite esse comportamento.
+console.log(nome);
