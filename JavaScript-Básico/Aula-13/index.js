f756cc4daf9c0166c740ad71384a941c9eb749de
Wadson-Ferreira <dev.wadson@gmail.com>
2026-01-02 20:38:23 -0300

    feat (aula-13) Implementa exemplos de estudos de Manipulação de String

---
+++ JavaScript-Básico/Aula-13/index.js
@@ -0,0 +1,20 @@
+let umaString = 'Um texto';
+ 
+console.log(umaString [3]); 
+
+console.log(umaString.charAt(3)); 
+
+console.log(umaString.concat(' em um lindo dia.'));
+
+console.log(umaString.indexOf('o'));
+
+console.log (umaString.replace('Um', 'Outro'));
+
+console.log (umaString.length);
+
+console.log(umaString.split(' '));
+
+console.log(umaString.toLowerCase());
+
+console.log(umaString.toUpperCase());
+
