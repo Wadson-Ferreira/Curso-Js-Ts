015892220ac0b8d7eb75a928c3f34b2e12e81c1b
Wadson-Ferreira <dev.wadson@gmail.com>
2026-01-03 16:46:20 -0300

    feat (aula-15) implementa exemplos de estudos do Objeto Math e Operações Matemáticas

---
+++ JavaScript-Básico/Aula-15/index.js
@@ -0,0 +1,23 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1); // arredonda o valor para baixo
+let num3 = Math.ceil(num1); // arredonda o valor para cima
+let num4 = Math.round(num1); // arredonda para o mais proxímo 
+
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+console.log(Math.max(1,2,3,4,5,-10,-50,1500, 9, 8, 7, 6)); //retorna o maior de uma sequência.
+console.log(Math.min(1,2,3,4,5,-10,-50,1500, 9, 8, 7, 6)); // retorna o menor de uma sequência.
+
+console.log(Math.PI); // Retorna o valor de pi até a 15ª casa
+
+console.log(Math.pow(10,2)); //calcula a potenciação e retorna um number
+
+console.log(9 ** (1/2)); // Retorna a raz quadrada de um número
+
+console.log(9 ** 0.5); // Retorna a raz quadrada de um número
+
+console.log(Math.sqrt(9)); // Retorna a raz quadrada de um número
+
+console.log(100/0); // Retorna um valor Infinity
