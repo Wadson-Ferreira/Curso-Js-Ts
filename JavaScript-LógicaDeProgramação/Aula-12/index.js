38fdcade3163e1d72ad73891e54b30ab9cd2e372
Wadson-Ferreira <dev.wadson@gmail.com>
2026-02-10 20:45:47 -0300

    feat (Aula-11) Implementa código de estudos sobre atribuição via desestruturação de Arrays

---
+++ JavaScript-LógicaDeProgramação/Aula-12/index.js
@@ -0,0 +1,8 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [primeiroNumero, segundoNumero, ...resto] = numeros;
+const [um, ,tres, , cinco, , sete] = numeros;
+
+console.log(primeiroNumero, segundoNumero);
+console.log(resto);
+console.log(um, tres, cinco, sete);
+
