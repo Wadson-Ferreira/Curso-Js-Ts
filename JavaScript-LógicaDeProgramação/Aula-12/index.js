b7d66e69aa3166c4c48cf48edab273d3d2936bb4
Wadson Ferreira <dev.wadson@gmail.com>
2026-07-21 15:46:05 -0300

    chore: Reestruturação de branches

--- JavaScript-LógicaDeProgramação/Aula-12/index.js
+++
@@ -1,8 +0,0 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const [primeiroNumero, segundoNumero, ...resto] = numeros;
-const [um, ,tres, , cinco, , sete] = numeros;
-
-console.log(primeiroNumero, segundoNumero);
-console.log(resto);
-console.log(um, tres, cinco, sete);
-
